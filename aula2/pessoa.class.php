3e65015dfd9636478e24ab8e1ba4860a1cb4dcd6
Gabriel Lima Brito da Silva <gabriellimab@gmail.com>
2022-11-03 21:32:57 -0300

    feat: atualiza aula2

---
+++ aula2/pessoa.class.php
@@ -0,0 +1,30 @@
+<?php
+class Pessoa{
+	//Atributos
+	private $nome;
+	private $idade;
+
+	//Métodoas Acessores
+	public function getNome(){
+		return $this->nome;
+	}	
+
+	public function setNome($nome){
+		$this->nome	= $nome;
+	}
+
+	public function getIdade(){
+		return $this->idade;
+	}
+
+	public function setIdade($idade){
+		$this->idade = $idade;	
+	}
+
+	//Método de Cálculo
+	public function calcularMeses(){
+		return $this->idade * 12;
+	}	//fecha calcularMeses
+
+}//fim da classe Pessoa
+?>
---
+++ aula2/pessoacontrole.php
@@ -0,0 +1,16 @@
+<?php
+//importando a classe pessoa
+include 'pessoa.class.php';
+
+//instanciando a classe pessoa.class
+$p = new Pessoa();
+
+//Setando os dados
+$p->setNome($_POST['txtnome']);
+$p->setIdade($_POST['txtidade']);
+
+//Mostrando os dados
+echo '<br> nome: '.$p->getNome().
+	 '<br> idade: '.$p->getIdade().	
+	 '<br> idade em meses: '.$p->calcularMeses();
+?>
