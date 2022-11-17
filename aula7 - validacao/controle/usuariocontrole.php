bbb703ca691b1f174647ada587e6ea5378f6ed1f
Gabriel Lima Brito da Silva <gabriellimab@gmail.com>
2022-11-16 21:41:31 -0300

    feat: inicia validação no controle

--- aula7 - validacao/controle/usuariocontrole.php
+++ aula7 - validacao/controle/usuariocontrole.php
@@ -6,23 +6,55 @@ include '../dao/usuariodao.class.php';
 
 switch ($_GET['op']) {
     case 'cadastrar':
-        //Cadastro sem validação
-        $usuario = new Usuario();
-        $usuario->login = $_POST['txtlogin'];
-        $usuario->senha = $_POST['txtsenha'];
-        $usuario->tipo = $_POST['seltipo'];
 
-        //Enviando o objeto para o bd
-        $usuarioDao = new UsuarioDAO();
-        $usuarioDao->cadastrarUsuario($usuario);
+        if (isset($_POST['txtlogin'])
+        && isset($_POST['txtsenha'])
+        && isset($_POST['seltipo'])
+        ) {
+            //TODO: validar as paradas
+            $erros = array();
+            if (!Validacao::validaLogin($_POST['txtlogin'])
+            ) {
+                $erros[] = 'login inválido.';
+            }
+            if (!Validacao::validaSenha($_POST['txtsenha'])
+            ) {
+                $erros[] = 'senha inválida.';
+            }
+            if (!Validacao::validaTipo($_POST['seltipo'])
+            ) {
+                $erros[] = 'tipo inválido.';
+            }
+            if (count($erros) == 0) {
+                //Cadastro com validação
+                $usuario = new Usuario();
+                $usuario->login = $_POST['txtlogin'];
+                $usuario->senha = $_POST['txtsenha'];
+                $usuario->tipo = $_POST['seltipo'];
+
+                //Enviando o objeto para o bd
+                $usuarioDao = new UsuarioDAO();
+                $usuarioDao->cadastrarUsuario($usuario);
+
+                $_SESSION['msg'] = 'Usuário Cadastrado!';
+                $_SESSION['usuario'] = serialize($usuario);
+
+                header('location:../visao/guiresposta.php');
+            }
+            else {
+                $_SESSION['usuario'] = serialize($usuario);
+
+                header('location:../visao/guiresposta.php');
+            }
+        }
+        else {
+            echo 'Variáveis inválidas!';
+        }
 
 
-        $_SESSION['usuario'] = serialize($usuario);
-
-        header('location:../visao/guiresposta.php');
         break;
 
-    case 'consultar':
+    case 'buscar':
         # code...
         break;
 
