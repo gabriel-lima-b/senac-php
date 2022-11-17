6ff5ecfd50623126da15153e8b71e7733c825568
Gabriel Lima Brito da Silva <gabriellimab@gmail.com>
2022-11-16 21:19:16 -0300

    feat: cria aula 7 com validação

---
+++ aula7 - validacao/visao/guierro.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+Design by Free CSS Templates
+http://www.freecsstemplates.org
+Released for free under a Creative Commons Attribution 2.5 License
+
+Name       : Keyboard 
+Description: A two-column, fixed-width design with dark color scheme.
+Version    : 1.0
+Released   : 20120915
+
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <!-- InstanceBegin template="Templates/modelo.dwt" codeOutsideHTMLIsLocked="false" -->
+  <head>
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>Erro</title>
+    <link
+      href="http://fonts.googleapis.com/css?family=Oswald:400,300"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <link
+      href="http://fonts.googleapis.com/css?family=Abel"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <link
+      href="../estilos/style.css"
+      rel="stylesheet"
+      type="text/css"
+      media="screen"
+    />
+  </head>
+  <body>
+    <div id="wrapper">
+      <div id="header-wrapper">
+        <div id="header" class="container">
+          <div id="logo">
+            <h1><a href="#">Site exemplo</a></h1>
+          </div>
+          <div id="menu">
+            <ul>
+              <li class="current_page_item">
+                <a href="../index.html">Homepage</a>
+              </li>
+              <li><a href="guicadusuario.html">Cadastro</a></li>
+              <li>
+                <a href="../controle/usuariocontrole.php?op=consultar">Consulta</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div id="banner">
+          <div class="content">
+            <img
+              src="../imagens/img02.jpg"
+              width="1000"
+              height="300"
+              alt="teclado"
+            />
+          </div>
+        </div>
+      </div>
+      <!-- end #header -->
+
+      <div id="page">
+        <div id="content">
+          <div class="post">
+            <!-- InstanceBeginEditable name="conteúdo" -->
+            <h2 class="title">Erro</h2>
+
+            <!-- InstanceEndEditable -->
+          </div>
+        </div>
+        <!-- end #content -->
+
+        <div id="sidebar">
+          <ul>
+            <li>
+              <h2>Categorias</h2>
+              <ul>
+                <li><a href="#">link 1</a></li>
+                <li><a href="#">link 2</a></li>
+                <li><a href="#">link 3</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <!-- end #sidebar -->
+        <div style="clear: both">&nbsp;</div>
+      </div>
+      <!-- end #page -->
+    </div>
+    <div id="footer">
+      <p>
+        Copyright (c) 2022 Sitename.com. All rights reserved. Design by
+        <a href="http://www.freecsstemplates.org">FCT</a>. Photos by
+        <a href="http://fotogrph.com/">Fotogrph</a>.
+      </p>
+    </div>
+    <!-- end #footer -->
+  </body>
+  <!-- InstanceEnd -->
+</html>
